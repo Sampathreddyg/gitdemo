d12750bbbf8637b0256de21e899dce4758cfa45a
unknown <sampathreddy.guttireddy@gmail.com>
2024-10-09 21:22:30 +0530

    Demo Project

---
+++ src/test/java/SeleniumDemo1/Demo1.java
@@ -0,0 +1,20 @@
+package SeleniumDemo1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+
+		
+		ChromeOptions opt = new ChromeOptions();
+		opt.addArguments("start-maximized");
+		WebDriver driver = new ChromeDriver(opt);
+		driver.get("https://google.com");
+		driver.close();
+		
+	}
+
+}
