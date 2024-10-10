843f29ba8c82b1610e69aa6c43d3155130ff86f4
sampathreddyg <sampathreddy.guttireddy@gmail.com>
2024-10-10 13:07:28 +0530

    swapping function

--- src/test/java/SeleniumDemo2/Sample1.java
+++ src/test/java/SeleniumDemo2/Sample1.java
@@ -6,19 +6,20 @@ public class Sample1 {
 
 	public static void main(String[] args) {
 
-		int a, b;
-
+		swapping2Numbers(103,397);
+	}
+	
+	public static void swapping2Numbers(int a, int b) {
 		Scanner s = new Scanner(System.in);
-		System.out.println("Enter first number : ");
-		a = s.nextInt();
-		System.out.println("Enter sencond number : ");
-		b = s.nextInt();
+//		System.out.println("Enter first number : ");
+//		a = s.nextInt();
+//		System.out.println("Enter sencond number : ");
+//		b = s.nextInt();
 		System.out.println("Numbers before swapping " + a + " and " + b);
 		a = a + b;
 		b = a - b;
 		a = a - b;
 		System.out.println("Numbers after swapping " + a + " and " + b);
-		
 	}
 
 }
